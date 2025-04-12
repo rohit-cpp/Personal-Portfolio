c5d19755a241d2e9d475aa74454d0c9aa7c2c557
Rohit <rrgawanderohit@gmail.com>
2025-04-12 14:22:41 +0530

    Navbar done

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useState } from "react";
+import logo from "../assets/logo.png";
+import { NAVIGATION_LINKS } from "../constants";
+import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -20,7 +24,76 @@ const Navbar = () => {
     }
     setIsMobileMenuOpen(false);
   };
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="fixed left-0 right-0 top-4 z-50 ">
+        {/* {Desktop Menu} */}
+        <div className="mx-auto hidden max-w-2xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/20 py-3 backdrop-blur-lg lg:flex">
+          <div className="flex items-center justify-between gap-6">
+            <div>
+              {" "}
+              <a href="#">
+                <img src={logo} className="w-45" alt="" />
+              </a>
+            </div>
+            <div>
+              <ul className="flex items-center gap-4">
+                {NAVIGATION_LINKS.map((item, index) => (
+                  <li key={index}>
+                    <a
+                      className="text-sm hover:text-yellow-400"
+                      href={item.href}
+                      onClick={(e) => handleLinkClick(e, item.href)}
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* mobile menu */}
+        <div className="rounded-lg backdrop-blur-md lg:hidden">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="#">
+                <img src={logo} className="m-2 ml-15 w-40" alt="" />
+              </a>
+            </div>
+            <div className="flex items-center">
+              <button
+                className="focus:outline-none lg:hidden cursor-pointer"
+                onClick={toggleMobileMenu}
+              >
+                {isMobileMenuOpen ? (
+                  <FaTimes className="m-2 h-6 w-5" />
+                ) : (
+                  <FaBars className="m-2 h-6 w-5 " />
+                )}
+              </button>
+            </div>
+          </div>
+          {isMobileMenuOpen && (
+            <ul className="ml-4 mt-4 flex flex-col gap-4 backdrop-blur-md ">
+              {NAVIGATION_LINKS.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    className="block w-full text-lg hover:text-yellow-400"
+                    onClick={(e) => handleLinkClick(e, href)}
+                  >
+                    {" "}
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
